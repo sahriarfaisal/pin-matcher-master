b4c8ddc20228a780e19a7f50709f38edc3557197
sahriarfaisal.cse@gmail.com <sahriarfaisal.cse@gmail.com>
2020-08-06 11:59:07 +0600

    PinMatcherDone

--- pinMatcher.js
+++ pinMatcher.js
@@ -1,19 +1,34 @@
 
-        //PinMatch button event handler,
+        //PinMatch Button Event Handler.
         const pinBtn = document.getElementById("generatePinBtn");
         pinBtn.addEventListener("click", function () {
-        document.getElementById("generateDisplay").value = getRandIntValue;
+            function getRandomInt() {
+                let random = Math.floor((Math.random() * 9000) + 1000);
+                let randomNum = parseInt(random);
+                return randomNum;
+            };
+            let getRandIntValue = getRandomInt();
+            document.getElementById("generateDisplay").value = getRandIntValue;
         });
-        //Random Number generate .
-        function getRandomInt() {
-            let random = Math.floor((Math.random() * 9000) + 1000);
-            let randomNum = parseInt(random);
-            return randomNum;
-        };
+        //Submit Button Event Handler.
+        const submitBtnMass = document.getElementById("submitBtn");
+        submitBtnMass.addEventListener("click", function () {
+            const calScreen = document.getElementById("screenValue").value;
+            let calScreenNum = parseInt(calScreen);
 
-// // Display
-//     function displayPass(x) {
-//         let ok = parseInt(x);
-//         form.screen.value = ok;
-           
-//        };
+            const randScreen = document.getElementById("generateDisplay").value;
+            let randScreenNum = parseInt(randScreen);
+        //Message show Condition.
+            if (randScreenNum == calScreenNum) {
+                const notifySection1 = document.getElementById("notifyFalse");
+                notifySection1.style.display = "none";
+                const notifySection2 = document.getElementById("notifyTrue");
+                notifySection2.style.display = "block";
+            } else {
+                const notifySection1 = document.getElementById("notifyFalse");
+                notifySection1.style.display = "block";
+                const notifySection2 = document.getElementById("notifyTrue");
+                notifySection2.style.display = "none";
+            }
+        });
+
